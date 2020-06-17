19fe830c40f25601be1429b7b2bdea17df39b8ad
Sacha Bruttin <sacha@bruttin.com>
2020-06-17 14:35:21 +0200

    Update index.js

--- afun95/demos/functionapp-demo/SimpleHttpTrigger/index.js
+++ afun95/demos/functionapp-demo/SimpleHttpTrigger/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello, it's still me " + (req.query.name || req.body.name)
+            body: "Hey, it's still me " + (req.query.name || req.body.name)
         };
     }
     else {
